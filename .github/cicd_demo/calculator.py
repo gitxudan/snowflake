b9d8c02bfe9e0eaae8aa2ab963744820b8e63174
Xu Dan <xudanbetsy@gmail.com>
2026-05-10 20:36:17 +0800

    ci setup 1

--- .github/cicd_demo/calculator.py
+++ .github/cicd_demo/calculator.py
@@ -3,11 +3,4 @@ def addition(x:int, y:int) -> int:
 
 def subtraction(x:int, y:int) -> int:
     return x - y
-
-def multiplication(x:int, y:int) -> int:
-    return x * y
-
-def division(x:int, y:int) -> float:
-    if y == 0:
-        raise ValueError("Denominator cannot be zero.")
-    return x / y    
+   
